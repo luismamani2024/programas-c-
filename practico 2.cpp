37309a5b56ce41460f2caceaafb677612b722635
luismamani2024 <156382890+luismamani2024@users.noreply.github.com>
2024-01-11 21:54:20 -0400

    Add files via upload

---
+++ practico 2.cpp
@@ -0,0 +1,21 @@
+
+
+
+int main(int argc, char *argv[]) {
+	short int numero_entero_p;
+	unsigned short int numeric_entero_pl;
+	int numeric_entero;
+	float numero_decimal;
+	doublenumero_decimal_g;
+	//Asigando valores limite de los tipos de dato
+	numero_entero_p=32767;//valor limite del tipo de dato
+	cout<<numero entero pequeño:"<<numeric_entero_p<<endl;
+	numero_entero_pl=65535;//valor limite del tipo de dato 65535 65530
+	cout<<"numero entero pequeño positivo:"<<numeric_entero_pl<<endl;
+	//Asignar los valores maximos para unsigned int y int
+	numero_entero=2147483647
+	cout<<"numero entero:"<<numero_entero:"<<endl;
+	numero_entero_s=4294967295;
+	cout<<"numero entero positivo:"<<numero_entero_s <<endl;
+	return 0;
+}
